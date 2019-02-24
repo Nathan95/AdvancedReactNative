2911946e5875a38ecb4b0aee76fa3cfb70adbfe5
Nathan95 <nathan@mediazoo.tv>
2019-02-24 12:01:17 +0000

    minimal swiping functionality

--- AdvancedReactNative/swipe/App.js
+++ AdvancedReactNative/swipe/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Card, Button } from "react-native-elements";
 import Deck from "./src/Deck";
 
 const DATA = [
@@ -47,7 +48,18 @@ const DATA = [
 
 export default class App extends React.Component {
   renderCard(item) {
-    return <Text>{item.text}</Text>;
+    return (
+      <Card title={item.text} image={{ uri: item.uri }} key={item.id}>
+        <Text style={{ marginBottom: 10 }}>
+          I can customize the Card further.
+        </Text>
+        <Button
+          icon={{ name: "code" }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
+    );
   }
 
   render() {
